d5405fdf9a31e9a5cb6dd919699993ed6271c9ce
Luuk Oudshoorn <luuk@lupanas.nl>
2020-10-14 21:54:02 +0200

    Totalpower imaging script update

--- CLEAN_13CO/TotalPower/image_TP_data.py
+++ CLEAN_13CO/TotalPower/image_TP_data.py
@@ -1,7 +1,9 @@
-os.system('rm -rf *.image')
+### 13 CO
+
+os.system('rm -rf *.image *last *log')
 sdimaging(
-infiles            =  'TP_13CO.ms',
-outfile            =  "TP_13CO.image",
+infiles            =  ['ms_data_F1', 'ms_data_F2', 'ms_data_F3', 'ms_data_F4', 'ms_data_F5'],
+outfile            =  "TP_13CO_notconcat.image",
 overwrite          =  True,
 field              =  "30_Doradus",
 spw                =  "*",
@@ -9,9 +11,9 @@ antenna            =  "",
 scan               =  "",
 intent             =  "OBSERVE_TARGET#ON_SOURCE",
 mode               =  "velocity",
-nchan              =  300,
-start              =  "282.715km/s",
-width              =  "-250m/s",
+nchan              =  1100,
+start              =  "290km/s",
+width              =  "-83.33333333m/s",
 veltype            =  "radio",
 outframe           =  "lsrk",
 gridfunction       =  "SF",
@@ -19,9 +21,9 @@ convsupport        =  6,
 truncate           =  -1,
 gwidth             =  -1,
 jwidth             =  -1,
-imsize             =  [280, 320],
-cell               =  "3arcsec",
-phasecenter        =  'J2000 05h38m32.0 -69d02m18.0',
+imsize             =  [250, 250],
+cell               =  "2.5arcsec",
+phasecenter        =  'J2000 05h38m46.839 -69d04m08.354',
 projection         =  "SIN",
 ephemsrcname       =  "",
 pointingcolumn     =  "direction",
@@ -32,4 +34,4 @@ brightnessunit     =  "Jy/beam",
 clipminmax         =  False
 )
 
-imcontsub("TP_13CO.image",chans="0~15;260~299",linefile='TP_13CO.line.image',contfile='TP_13CO.res.image')
+imcontsub("TP_13CO_notconcat.image",chans="30~150;950~1080",linefile='TP_13CO_notconcat.line.image',contfile='TP_13CO_notconcat.res.image',fitorder=1)
