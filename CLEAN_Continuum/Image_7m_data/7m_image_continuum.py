11e8880ddd854e5572ea0d6f9d2d1301a8066fa9
Luuk Oudshoorn <luuk@lupanas.nl>
2020-11-09 15:15:41 +0100

    Added 7m continuum script

---
+++ CLEAN_Continuum/Image_7m_data/7m_image_continuum.py
@@ -0,0 +1,205 @@
+# step2: Imaging
+
+######################################################
+# Use original MS sets
+
+
+# Field 1: X2180
+
+
+vis7m_F1 = ["../../2019.1.00843.S/science_goal.uid___A001_X1465_X2180/group.uid___A001_X1465_X2181/member.uid___A001_X1465_X2184/calibrated/working/uid___A002_Xe1baa0_X8097.ms",
+	"../../2019.1.00843.S/science_goal.uid___A001_X1465_X2180/group.uid___A001_X1465_X2181/member.uid___A001_X1465_X2184/calibrated/working/uid___A002_Xe220f2_X4ea1.ms",
+	"../../2019.1.00843.S/science_goal.uid___A001_X1465_X2180/group.uid___A001_X1465_X2181/member.uid___A001_X1465_X2184/calibrated/working/uid___A002_Xe230a1_X28f6.ms",
+	"../../2019.1.00843.S/science_goal.uid___A001_X1465_X2180/group.uid___A001_X1465_X2181/member.uid___A001_X1465_X2184/calibrated/working/uid___A002_Xe230a1_X32eb.ms",
+	"../../2019.1.00843.S/science_goal.uid___A001_X1465_X2180/group.uid___A001_X1465_X2181/member.uid___A001_X1465_X2184/calibrated/working/uid___A002_Xe220f2_X5842.ms",
+	"../../2019.1.00843.S/science_goal.uid___A001_X1465_X2180/group.uid___A001_X1465_X2181/member.uid___A001_X1465_X2184/calibrated/working/uid___A002_Xe247d0_Xeb.ms", #!
+	"../../2019.1.00843.S/science_goal.uid___A001_X1465_X2180/group.uid___A001_X1465_X2181/member.uid___A001_X1465_X2184/calibrated/working/uid___A002_Xe220f2_X62c1.ms",
+	"../../2019.1.00843.S/science_goal.uid___A001_X1465_X2180/group.uid___A001_X1465_X2181/member.uid___A001_X1465_X2184/calibrated/working/uid___A002_Xe27761_X172c.ms"]
+
+# Field 2: X2188
+vis7m_F2 = ["../../2019.1.00843.S/science_goal.uid___A001_X1465_X2188/group.uid___A001_X1465_X2189/member.uid___A001_X1465_X218c/calibrated/working/uid___A002_Xe31981_Xf7ea.ms.split.cal",
+	"../../2019.1.00843.S/science_goal.uid___A001_X1465_X2188/group.uid___A001_X1465_X2189/member.uid___A001_X1465_X218c/calibrated/working/uid___A002_Xe32bed_Xdce2.ms.split.cal",
+	"../../2019.1.00843.S/science_goal.uid___A001_X1465_X2188/group.uid___A001_X1465_X2189/member.uid___A001_X1465_X218c/calibrated/working/uid___A002_Xe32bed_Xe889.ms.split.cal",
+	"../../2019.1.00843.S/science_goal.uid___A001_X1465_X2188/group.uid___A001_X1465_X2189/member.uid___A001_X1465_X218c/calibrated/working/uid___A002_Xe37224_X3836.ms.split.cal",
+	"../../2019.1.00843.S/science_goal.uid___A001_X1465_X2188/group.uid___A001_X1465_X2189/member.uid___A001_X1465_X218c/calibrated/working/uid___A002_Xe37224_X461a.ms.split.cal",
+	"../../2019.1.00843.S/science_goal.uid___A001_X1465_X2188/group.uid___A001_X1465_X2189/member.uid___A001_X1465_X218c/calibrated/working/uid___A002_Xe37224_Xdfe9.ms.split.cal",
+	"../../2019.1.00843.S/science_goal.uid___A001_X1465_X2188/group.uid___A001_X1465_X2189/member.uid___A001_X1465_X218c/calibrated/working/uid___A002_Xe37224_Xe70a.ms.split.cal"
+	]
+# Field 3: X21a0
+vis7m_F3 = ["../../2019.1.00843.S/science_goal.uid___A001_X1465_X21a0/group.uid___A001_X1465_X21a1/member.uid___A001_X1465_X21a4/calibrated/working/uid___A002_Xe1a561_X2fad.ms",
+	"../../2019.1.00843.S/science_goal.uid___A001_X1465_X21a0/group.uid___A001_X1465_X21a1/member.uid___A001_X1465_X21a4/calibrated/working/uid___A002_Xe1baa0_X477e.ms",
+	"../../2019.1.00843.S/science_goal.uid___A001_X1465_X21a0/group.uid___A001_X1465_X21a1/member.uid___A001_X1465_X21a4/calibrated/working/uid___A002_Xe1baa0_X7737.ms",
+	"../../2019.1.00843.S/science_goal.uid___A001_X1465_X21a0/group.uid___A001_X1465_X21a1/member.uid___A001_X1465_X21a4/calibrated/working/uid___A002_Xe1baa0_X7d43.ms"]
+# Field 4: X2198
+vis7m_F4 = ["../../2019.1.00843.S/science_goal.uid___A001_X1465_X2198/group.uid___A001_X1465_X2199/member.uid___A001_X1465_X219c/calibrated/working/uid___A002_Xe2ada9_X18144.ms",
+	"../../2019.1.00843.S/science_goal.uid___A001_X1465_X2198/group.uid___A001_X1465_X2199/member.uid___A001_X1465_X219c/calibrated/working/uid___A002_Xe2ada9_X18ea1.ms",
+	"../../2019.1.00843.S/science_goal.uid___A001_X1465_X2198/group.uid___A001_X1465_X2199/member.uid___A001_X1465_X219c/calibrated/working/uid___A002_Xe31981_X3cee.ms",
+	"../../2019.1.00843.S/science_goal.uid___A001_X1465_X2198/group.uid___A001_X1465_X2199/member.uid___A001_X1465_X219c/calibrated/working/uid___A002_Xe31981_Xebf5.ms",
+	"../../2019.1.00843.S/science_goal.uid___A001_X1465_X2198/group.uid___A001_X1465_X2199/member.uid___A001_X1465_X219c/calibrated/working/uid___A002_Xe2ada9_X187a8.ms",
+	"../../2019.1.00843.S/science_goal.uid___A001_X1465_X2198/group.uid___A001_X1465_X2199/member.uid___A001_X1465_X219c/calibrated/working/uid___A002_Xe31981_X30e4.ms",
+	"../../2019.1.00843.S/science_goal.uid___A001_X1465_X2198/group.uid___A001_X1465_X2199/member.uid___A001_X1465_X219c/calibrated/working/uid___A002_Xe31981_X47c7.ms",
+	"../../2019.1.00843.S/science_goal.uid___A001_X1465_X2198/group.uid___A001_X1465_X2199/member.uid___A001_X1465_X219c/calibrated/working/uid___A002_Xe32bed_X3f50.ms"]
+# Field 5: X2190
+vis7m_F5 = ["../../2019.1.00843.S/science_goal.uid___A001_X1465_X2190/group.uid___A001_X1465_X2191/member.uid___A001_X1465_X2194/calibrated/working/uid___A002_Xe3da01_X305b.ms",
+	"../../2019.1.00843.S/science_goal.uid___A001_X1465_X2190/group.uid___A001_X1465_X2191/member.uid___A001_X1465_X2194/calibrated/working/uid___A002_Xe407cf_X9d8c.ms",
+	"../../2019.1.00843.S/science_goal.uid___A001_X1465_X2190/group.uid___A001_X1465_X2191/member.uid___A001_X1465_X2194/calibrated/working/uid___A002_Xe3da01_X16c5.ms",
+	"../../2019.1.00843.S/science_goal.uid___A001_X1465_X2190/group.uid___A001_X1465_X2191/member.uid___A001_X1465_X2194/calibrated/working/uid___A002_Xe407cf_X17cba.ms",
+	"../../2019.1.00843.S/science_goal.uid___A001_X1465_X2190/group.uid___A001_X1465_X2191/member.uid___A001_X1465_X2194/calibrated/working/uid___A002_Xe407cf_Xbb9d.ms",
+	"../../2019.1.00843.S/science_goal.uid___A001_X1465_X2190/group.uid___A001_X1465_X2191/member.uid___A001_X1465_X2194/calibrated/working/uid___A002_Xe3da01_X2352.ms",
+	"../../2019.1.00843.S/science_goal.uid___A001_X1465_X2190/group.uid___A001_X1465_X2191/member.uid___A001_X1465_X2194/calibrated/working/uid___A002_Xe407cf_X20de.ms",
+	"../../2019.1.00843.S/science_goal.uid___A001_X1465_X2190/group.uid___A001_X1465_X2191/member.uid___A001_X1465_X2194/calibrated/working/uid___A002_Xe407cf_Xf987.ms"]
+
+
+######################################################
+# Parameters:
+
+Nchans = 1
+NumIter = 15000
+cleanthres = '0.5mJy'
+cellsize = '1.2arcsec'
+mapsize = [250,250,250,250,250]
+# We image each field seperately and have these phasecenters
+phasecenters = ['J2000 05h38m32.0 -69d02m18.0','J2000 05h38m34.0 -69d04m38.0','J2000 05h39m0.0 -69d02m45.0','J2000 05h39m0.0 -69d04m42.0','J2000 05h38m36.0 -69d07m00.0']
+
+###########################################################
+
+
+
+# Only H30a window:18
+spw = "18:10~700,18:1300~1910"
+visdata = [vis7m_F1,vis7m_F2,vis7m_F3,vis7m_F4,vis7m_F5]
+imagenames = ['30DOR_F'+str(w)+"_cont_spw_18_spw_nterms2" for w in range(1,6)]
+targetdirs = ['./field'+str(w)+'/' for w in range(1,6)]
+for i in [0,1,2,3,4]:
+    if i==2:
+        spws = 2*[spw]
+    else:
+        spws = spw
+    target_dir = targetdirs[i]
+    out_file = target_dir + imagenames[i]
+    tclean(vis=visdata[i],
+        imagename = out_file,
+        field = '30_Doradus',
+        spw = spws,
+        specmode = 'mfs',
+        outframe = 'LSRK',
+        niter = NumIter,
+        threshold = cleanthres,
+        deconvolver = 'hogbom',
+        gridder = 'mosaic',
+        imsize = mapsize[i],
+        cell = cellsize,
+        weighting = 'briggs',
+        robust = 0.5,
+        restoringbeam=["7.5arcsec","7.5arcsec","0deg"],
+        phasecenter = phasecenters[i],
+        #uvtaper=['10arcsec','10arcsec','0deg'],
+        parallel=False,
+        nterms=2,
+        pbcor = False)
+
+
+
+
+
+
+
+
+
+
+
+
+# MOSAICS
+# Weighted mosaics using python
+import numpy as np
+from astropy.stats import mad_std
+from astropy import wcs
+from astropy.io import fits
+from reproject.mosaicking import find_optimal_celestial_wcs
+from reproject import reproject_interp
+from reproject.mosaicking import reproject_and_coadd
+from FITS_tools import regrid_cube
+from FITS_tools.downsample import *
+from glob import glob
+
+imlist = glob('../field?/*cont_spw_27_spw_taper0arcsec.image')
+for im in imlist:
+    outfile = './fitsfiles/'+im.split('/')[-1].replace('.image','.fits')
+    if not os.path.exists(outfile):        
+        exportfits(im, outfile, velocity=True,dropdeg=True,overwrite=True)
+
+imlist = glob('../field?/*cont_spw_27_spw_taper0arcsec.pb')
+for im in imlist:
+    outfile = './fitsfiles/'+im.split('/')[-1].replace('.pb','.pb.fits')
+    if not os.path.exists(outfile):        
+        exportfits(im, outfile, velocity=True,dropdeg=True,overwrite=True)
+
+
+
+
+# Python: get var maps
+def get_var_map(im):
+    flatimg, hdr = fits.getdata(im,header=True)
+    rms = mad_std(flatimg, axis=None, ignore_nan=True)
+    pbfile = im.replace('.fits','.pb.fits')
+    pbimage, pbhd = fits.getdata(pbfile,header=True)
+    pbcorimg = flatimg / pbimage
+    varimg  = (rms / pbimage)**2
+    fits.writeto(im.replace('.fits','.var.fits'),
+                     varimg.astype(np.float32), hdr, overwrite=True)
+
+imlist = glob('./fitsfiles/*arcsec.fits')
+for im in imlist:
+    get_var_map(im,)
+
+
+hd2d = fits.getheader('./12m_continuum.model.fits')
+naxis2 = hd2d['naxis2']
+naxis1 = hd2d['naxis1']
+
+
+naxis3 = 1
+imlist = glob('./fitsfiles/*arcsec.fits')
+outputnames='30Dor_continuum'
+
+mcube = np.zeros(shape=(naxis2,naxis1))
+foot  = np.zeros(shape=(naxis2,naxis1))
+wtnse = np.zeros(shape=(naxis2,naxis1))
+
+varlist = [w.replace('.fits','.var.fits') for w in imlist]
+N_ims = len(imlist)
+hdu_cube = [None] * N_ims
+hdu_slic = [None] * N_ims
+var_cube = [None] * N_ims
+var_slic = [None] * N_ims
+wcs_obj = [None] * N_ims
+for i, im in enumerate(imlist):
+    hdu_cube[i] = fits.open(imlist[i])[0]
+    var_cube[i] = fits.open(varlist[i])[0]
+
+
+
+variance_wts = []
+for i, im in enumerate(imlist):
+    hdu_slic[i] = fits.PrimaryHDU(data=hdu_cube[i].data, header=hdu_cube[i].header)
+    var_slic[i] = fits.PrimaryHDU(data=var_cube[i].data, header=var_cube[i].header)
+    variance_wts.append(1/var_slic[i].data)
+    wcs_obj[i] = wcs.WCS(hdu_slic[i])
+data_tuple = [None]*N_ims
+for i, im in enumerate(imlist):
+    data_tuple[i] = (hdu_slic[i].data,wcs_obj[i])
+mcube, foot = reproject_and_coadd(data_tuple, hd2d,input_weights=variance_wts,
+				        reproject_function=reproject_interp)
+
+fits.writeto(outputnames+'.mosaic.fits',mcube.astype(np.float32), hd2d, overwrite=True)
+fits.writeto(outputnames+'.foot.fits',foot.astype(np.float32), hd2d, overwrite=True)
+foot_sqrt = np.sqrt(foot)
+
+
+
+foot_sqrt[foot_sqrt==0] = np.nan
+wtnse = np.nanmean(1/foot_sqrt, axis=0, keepdims=True)
+fits.writeto(outputnames+'.rms.fits',wtnse.astype(np.float32), hd2d, overwrite=True)
+
+
+
