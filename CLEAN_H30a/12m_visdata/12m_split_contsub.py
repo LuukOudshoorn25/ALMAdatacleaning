e3cc6f1e4fb04d97eec68bf6b7396fd000ee1d37
Luuk Oudshoorn <luuk@lupanas.nl>
2020-11-09 10:22:43 +0100

    Spllitting of CO emission and redoing contsub

--- CLEAN_H30a/12m_visdata/12m_split_contsub.py
+++ CLEAN_H30a/12m_visdata/12m_split_contsub.py
@@ -11,13 +11,26 @@ mslist = ['uid___A002_Xe3da01_X18fa.ms',
 
 for ms in mslist:
     print(ms)
-    if not os.path.exists(ms+".split27"):
-        fpath = glob('../../2019*/science*/*/*/calibrated/'+ms+'*')[0]
-        print('Splitting',ms)
-        split(vis=fpath,spw="27",field="30_Dor*",outputvis=ms+".split27")
-        print('UVcontsub',ms)
-        uvcontsub(vis=ms+".split27",field="30_Dor*",fitspw="0:230.8~231.1GHz;232.4~232.48GHz")
-
-concat(glob('*.contsub'),'H30a_12m.contsub.concat.ms')
+        #fpath = glob('../../2019*/science*/*/*/calibrated/'+ms+'*')[0]
+        #print('Splitting',ms)
+        #split(vis=fpath,spw="27",field="30_Dor*",outputvis=ms+".split27")
+        #print('UVcontsub',ms)
+    uvcontsub(vis=ms+".split27",field="30_Dor*",fitspw="*:230.688~231.5GHz;231.95~232.687GHz",fitorder=1)
 
 
+
+
+
+
+
+
+#concat(glob('*.contsub'),'H30a_12m.contsub.concat.ms')
+
+for ms in glob('*.split27'):
+    print('UVcontsub',ms)
+    split(vis=ms,field="30_Dor*",spw='0:231.47~232.55GHz',outputvis=ms+'.splitchan',datacolumn='data')
+
+
+for ms in glob('*.split27.splitchan'):
+    print('UVcontsub',ms)
+    uvcontsub(vis=ms,field="30_Dor*",fitspw="*:231.47~231.6GHz;231.95~232.5GHz",fitorder=0)
